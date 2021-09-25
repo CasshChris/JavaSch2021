388272c0dfe3cfd0f2d958324df00986d2bde663
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-09-25 17:57:21 -0500

    Assignment 1.5 finished & added git ignore file

---
+++ wk1/Assignment_1-5/ComputeExpression.java
@@ -0,0 +1,14 @@
+// Assignment 1.5
+// this program will compute the expression 9.5 x 4.5 - 2.5 x 3 / 45.5 - 3.5 
+
+public class ComputeExpression {
+
+    public static void main(String[] args) {
+        // preform the calcutlation
+        double calculation = 9.5 * 4.5 - 2.5 * 3 / 45.5 - 3.5;
+
+        // Output the expression
+        System.out.println("the Answer for the expression is " + calculation);
+    }
+
+}
