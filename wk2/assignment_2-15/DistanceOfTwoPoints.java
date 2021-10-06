c95bd85c88abe7720b9e43466d78fe1bd232d2ce
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-10-06 10:08:37 -0500

    wk2 add

--- wk2/assignment_2-15/DistanceOfTwoPoints.java
+++ wk2/assignment_2-15/DistanceOfTwoPoints.java
@@ -8,36 +8,30 @@ public class DistanceOfTwoPoints {
     public static void main(String[] args){
         // create a scanner object
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Find the distance of two points using \"√(x2 - x1)^2 + (y2 - y1)\"\n");
 
-        // prompt the user for the first point
+        // prompt the user to input the first point
         System.out.println("Enter the first point");
-        System.out.print("x1: ");
-        double x1 = scan.nextDouble();
-        System.out.println(); // blank line
-        System.out.print("y1: ");
-        double y1 = scan.nextDouble();
-        System.out.println(); // blank line
+        System.out.print("x1, y1: ");
+        double x1 = scan.nextDouble(); // 1.5
+        double y1 = scan.nextDouble(); // 4
         
-        // prompt the user for the second point
+        // prompt the user to input the second point
         System.out.println("Enter the second point");
-        System.out.print("x2: ");
-        double x2 = scan.nextDouble();
-        System.out.println(); // blank line
-        System.out.print("y2: ");
-        double y2 = scan.nextDouble();
-        System.out.println(); // blank line
+        System.out.print("x2, y2: ");
+        double x2 = scan.nextDouble(); // -3.9
+        double y2 = scan.nextDouble(); // 5
 
-        // close the scanner object
+        // close scanner to not waste memory
         scan.close();
 
         // Calculate x's & y's
         double xCalc = Math.pow((x2 - x1), 2);
         double yCalc = Math.pow((y2 - y1), 2);
-        double finalCalc = xCalc + yCalc;
 
         // find the sqare root
-        double sqrtCalc = Math.sqrt(finalCalc);
+        double sqrtCalc = Math.sqrt(xCalc + yCalc); // 8.764...
 
         // output the result of the equation
         System.out.printf("The distance between two points is %f", sqrtCalc);
