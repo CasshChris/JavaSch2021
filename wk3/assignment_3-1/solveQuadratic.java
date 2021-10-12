2173cd8f33fe9ffe1b483c65120fd1c7308a4267
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-10-12 14:20:06 -0500

    rps.java done ✅

--- wk3/assignment_3-1/solveQuadratic.java
+++ wk3/assignment_3-1/solveQuadratic.java
@@ -1,11 +1,56 @@
-// assignment 3.1
+// Assignment 3.1 - Solve the quadratic
+// 
+
 import java.util.Scanner;
 import java.lang.Math;
 
 public class solveQuadratic {
 
     public static void main(String[] args){
-        // 
-    } 
+        // create Scanner class
+        Scanner scan = new Scanner(System.in);
+
+        // prompt the user for the a, b, & c data & store it
+        System.out.println("Solve the quadratic equation");
+        System.out.print("Enter a, b, c: ");
+        double a = scan.nextDouble(); // 1.0, 1, 1
+        double b = scan.nextDouble(); // 3, 2.0, 2
+        double c = scan.nextDouble(); // 1, 1, 3
+
+        // use the quadratic() to find the answer 
+        quadratic(a, b, c);
+        
+    }
+    
+    // this method calculates 
+    public static void quadratic(double a, double b, double c) {
+        // calculate the discriminate
+        double discriminate = Math.pow(b, 2) - (4 * a * c);
+        
+        // if the discriminate is less then zero it is negative - no solotion
+        // if the discriminate is more then zero it is positive - 2 solotions
+        // if the discriminate is equal to zero - 1 solotion / the discriminate is removed
+        if (discriminate > 0) {
+            // calculate the roots
+            double r1 = - b + Math.sqrt(discriminate) / (2 * a);
+            double r2 = - b - Math.sqrt(discriminate) / (2 * a);
+            
+            // output the quadratics
+            System.out.printf("The equation has two roots: %f and %f ", r1, r2); 
+            
+        } else if (discriminate == 0) {
+            // calcule the root
+            double root = - b / (2 * a);
+
+            // output the quadratic
+            System.out.printf("The equation has one root: %f ", root);
+
+        } else if (discriminate < 0) {
+            // tell the use there is no root
+            System.out.println("The equation has no roots");
+
+        }
+
+    }
 
 }
--- wk3/assignment_3-17/RPS.java
+++ wk3/assignment_3-17/RPS.java
@@ -11,29 +11,32 @@ public class RPS {
         Scanner scan = new Scanner(System.in);
 
         // create Random object
-        // Random random = new Random();
+        Random random = new Random();
 
         // create a random number
-        // int compNumber = random.nextInt(3);
-        int compNumber = 3;
+        // int compNumber = random.nextInt(4);
+        int compNumber = 2;
 
         // create a array to store rock, paper, scissor
-        String[] rpsString = {"Scissors", "Rock", "Paper", "Scissors"};
+        String[] rpsString = {"Scissors", "Rock", "Paper"};
 
         // prompt user to input a number
-        System.out.print("Scissors (0), Rock (1), Paper (2)");
+        System.out.print("Scissors (0), Rock (1), Paper (2) : ");
         int userNumber = scan.nextInt();
 
-        // if the random number is greater then the users input, then the computer won
+        // if the random number is greater then the users input or if , then the computer won
         // if the random number is less then the users input, then the user won
-        if (compNumber > userNumber) {
+        if ((compNumber > userNumber) ^ ((compNumber == 0) && (userNumber == 2))) {
             System.out.printf("The computers answer is %s. Your answer was %s. The computer won!", rpsString[(int)compNumber], rpsString[(int)userNumber]);
-        } else if (userNumber > compNumber) {
+
+        } else if ((userNumber > compNumber) ^ ((userNumber == 0) && (compNumber == 2))) {
             System.out.printf("The computers answer is %s. Your answer was %s. The you won!", rpsString[(int)compNumber], rpsString[(int)userNumber]);
+
         } else {
             System.out.printf("The computers answer is %s. Your answer was %s. Tie.", rpsString[(int)compNumber], rpsString[(int)userNumber]);
+
         }
-        
+
     }
 
 }
