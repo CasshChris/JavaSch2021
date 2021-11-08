71f0767b06b137ea3af6ef86a08e4ccf6a6cc4de
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-11-08 12:15:27 -0600

    generateVihiclePlateNumber added

---
+++ wk7/generateVihiclePlateNumber.java
@@ -0,0 +1,26 @@
+public class generateVihiclePlateNumber {
+    public static void main(String[] args) {
+        // create StringBuilder object
+        StringBuilder plate = new StringBuilder();
+        
+        // using a for loop generate three characters
+        for (int i = 0; i < 3; i++) {
+            // create a random character
+            char character = (char)(Math.random() * 26 + 'A');
+
+            // append charascter to plate
+            plate.append(character);
+        }
+
+        // using a for loop generate four digits
+        for (int i = 0; i < 4; i++) {
+            // create a random digit
+            char digit = (char)(Math.random() * 10 + '0');
+
+            // append the digit to plate
+            plate.append(digit);
+        }
+
+        System.out.println("The generated vehicle plate number is: " + plate);
+    }
+}
