10c9e76b46423390658907a191c7d1018a517d8a
Christian Alvarez <Alvarez.christian46@outlook.com>
2021-11-17 19:54:13 -0600

    wk8 comments

--- wk8/computeDeviation.java
+++ wk8/computeDeviation.java
@@ -1,8 +1,52 @@
 // Assignment 7.11 - Compute deviation
-// this program will 
+// this program will find the mean & standard deviation
+import java.util.Scanner;
+import java.lang.Math;
 
 public class computeDeviation {
     public static void main(String[] args) {
+        // create scanner object
+        Scanner scan = new Scanner(System.in);
+
+        // create data intake array 
+        double[] numbers = new double[10];
+
+        // get the ten numbers
+        System.out.print("Enter 10 numbers: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scan.nextDouble(); // 1.9 2.5 3.7 2 1 6 3 4 5 2
+        }
+
+        // output mean & deviation
+        System.out.printf("The mean is: %.2f \n", mean(numbers));
+        System.out.printf("The standard deviation is: %.2f", deviation(numbers));
+
+        // close scanner object
+        scan.close();
+    }
+
+    public static double deviation(double[] numbers) {
+        // find the mean
+        double mean = mean(numbers);
+
+        // declare deviation
+        double deviation = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            deviation += Math.pow(numbers[i] - mean, 2);
+        }
+
+        // return √deviation / amount of points - 1
+        return Math.sqrt(deviation / (numbers.length - 1));
+    }
+
+    public static double mean(double[] numbers) {
         // 
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        // return the sum of the points / amount of points
+        return sum / numbers.length;
     }
 }
--- wk8/mergeTwoList.java
+++ wk8/mergeTwoList.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class mergeTwoList {
     public static void main(String[] args) {
-        
+        // create scanner object
         Scanner scan = new Scanner(System.in);
 
         // get first list and save to array
@@ -50,41 +50,44 @@ public class mergeTwoList {
     }
 
     public static int[] merge(int[] list1, int[] list2) {
-        // Create the merge list 
+        // Create the merge list array
         int[] merge = new int[list1.length + list2.length];
-        int m = 0, l1 = 0, l2 = 0;
-        boolean isList1Finished = false;
-        boolean isL2Finished = false;
 
+        // declare m, l1, l2, 
+        int m = 0;
+        int l1 = 0;
+        int l2 = 0;
+        boolean isL1F = false;
+        boolean isL2F = false;
+
+        // do while the l's are not done
         do {
+            // if the l's are equal to the list
             if (l1 == list1.length) {
-                isList1Finished = true;
+                isL1F = true;
             } 
             if (l2 == list2.length) {
-                isL2Finished = true;
+                isL2F = true;
             } 
 
-            if (isList1Finished && isL2Finished) {
+            // 
+            if (isL1F && isL2F) {
                 break;
             }
 
-            if (!isList1Finished && isL2Finished) {
+            // 
+            if (!isL1F && isL2F) {
                 merge[m++] = list1[l1++];
-            }
-            else if (!isL2Finished && isList1Finished) {
+            } else if (!isL2F && isL1F) {
                 merge[m++] = list2[l2++];
-            }
-            else if (list1[l1] <= list2[l2]) { 
+            } else if (list1[l1] <= list2[l2]) { 
                 merge[m++] = list1[l1++];
-            }
-            else if (list2[l2] <= list1[l1]) {
+            } else if (list2[l2] <= list1[l1]) {
                 merge[m++] = list2[l2++];
             }
 
-        } while (!isList1Finished || !isL2Finished);
+        } while (!isL1F || !isL2F);
 
         return merge;
     }
-
-
 }
